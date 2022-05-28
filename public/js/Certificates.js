1d2c3ebcb48a6ac0a0beefa96fe13061a9e1aae6
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2022-05-28 18:22:41 -0300

    feat: addition of functions responsible for interaction effects

--- public/js/Certificates.js
+++ public/js/Certificates.js
@@ -1,3 +1,55 @@
-function validateDescriptionSize() {
-  document
+function ShowClickEffect(tag) {
+  const tagID = tag.id;
+  let element;
+
+  switch (tagID) {
+    case "imgNode":
+      document.getElementById(tagID).style.transition = "all 100ms ease-in";
+      document.getElementById(tagID).style.opacity = "0.6";
+
+      element = document.getElementById("clickNode");
+      element.style.transition = "all 100ms ease-in";
+      element.style.visibility = "visible";
+      break;
+
+    case "imgGit":
+      document.getElementById(tagID).style.transition = "all 100ms ease-in";
+      document.getElementById(tagID).style.opacity = "0.6";
+
+      element = document.getElementById("clickGit");
+      element.style.transition = "all 100ms ease-in";
+      element.style.visibility = "visible";
+      break;
+
+    default:
+      break;
+  }
+}
+
+function RemoveClickEffect(tag) {
+  const tagID = tag.id;
+  let element;
+
+  switch (tagID) {
+    case "imgNode":
+      document.getElementById(tagID).style.transition = "all 100ms ease-in";
+      document.getElementById(tagID).style.opacity = "100";
+
+      element = document.getElementById("clickNode");
+      element.style.transition = "all 100ms ease-in";
+      element.style.visibility = "hidden";
+      break;
+
+    case "imgGit":
+      document.getElementById(tagID).style.transition = "all 100ms ease-in";
+      document.getElementById(tagID).style.opacity = "100";
+
+      element = document.getElementById("clickGit");
+      element.style.transition = "all 100ms ease-in";
+      element.style.visibility = "hidden";
+      break;
+
+    default:
+      break;
+  }
 }
