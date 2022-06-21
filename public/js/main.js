282184b99a85a4d4ec44bce91ece520f743fc8c9
Vítor Oliveira <61060367+vitorroliveiraa@users.noreply.github.com>
2022-06-21 15:17:25 -0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -15,7 +15,7 @@
       imagePath: "/img/nodejsBluee.png",
       imgWidth: 45,
       imgHeight: 42,
-      lineColor: "#1d2a36",
+      lineColor: "#1d2a",
       particleDensity: 0.2,
       particleChance: 0.2,
       particleVelocity: 60,
